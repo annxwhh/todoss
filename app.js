f923e8aa85caa8a3d9190cad64046fb73d4704cf
annxwhh <156710823+annxwhh@users.noreply.github.com>
2024-01-31 21:03:41 +0400

    ,

--- app.js
+++ app.js
@@ -15,7 +15,4 @@ function addTodo() {
 function updateTodoList() {
     const todoList = document.getElementById('todo-list');
     const todoCount = document.getElementById('todo-count');
-
-    todoList.innerHTML = ";
-    todoCount.innerHTML = Total Todos: ${todos.length}
 }
