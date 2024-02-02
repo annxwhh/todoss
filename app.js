f771fadb88ff432a698663a207a1540123b32cd9
annxwhh <156710823+annxwhh@users.noreply.github.com>
2024-02-02 21:03:48 +0400

    .

--- app.js
+++ app.js
@@ -18,4 +18,28 @@ function updateTodoList() {
     todoList.innerHTML = '';
     todoCount.innerText = `Total Todos: ${todos.length}`;
 
+    todos.forEach((todo, index) => {
+        const todoItem = document.createElement('li');
+        todoItem.classList.add('todo-item');
+
+        const editIcon = document.createElement('span');
+        editIcon.innerHTML = '&#9998;';
+        editIcon.classList.add('edit-icon');
+        editIcon.onclick = () => editTodo(index);
+
+        const todoText = document.createElement('span');
+        todoText.innerText = todo.text;
+
+        const label = document.createElement('span');
+        label.innerText = todo.status;
+        label.classList.add('label', todo.status.toLowerCase());
+        label.onclick = () => toggleStatus(index);
+
+        todoItem.appendChild(todoText);
+        todoItem.appendChild(editIcon);
+        todoItem.appendChild(label);
+
+        todoList.appendChild(todoItem);
+    });
 }
+
