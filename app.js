7afdcaa349817e5bd20c52f98a89819e98fc489c
annxwhh <156710823+annxwhh@users.noreply.github.com>
2024-02-13 22:13:27 +0400

    .

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 let todos = [];
 
+function submitTodoForm(event) {
+    event.preventDefault();
+    addTodo();
+}
+
 function addTodo() {
     const todoInput = document.getElementById('todo-input');
     const todoText = todoInput.value.trim();
@@ -12,42 +17,70 @@ function addTodo() {
 }
 
 function updateTodoList() {
+    const todoListContainer = document.getElementById('todo-list-container');
+    todoListContainer.style.display = 'block'; // Show the todo list container
+
     const todoList = document.getElementById('todo-list');
     const todoCount = document.getElementById('todo-count');
 
     todoList.innerHTML = '';
-    todoCount.innerText = `Total Todos: ${todos.length}`;
+    todoCount.innerText = `Todos: ${todos.length}`;
 
     todos.forEach((todo, index) => {
         const todoItem = document.createElement('li');
         todoItem.classList.add('todo-item');
 
+        const todoTextContainer = document.createElement('div');
+        todoTextContainer.classList.add('text-container');
+
         const editIcon = document.createElement('span');
         editIcon.innerHTML = '&#9998;';
         editIcon.classList.add('edit-icon');
-        editIcon.onclick = () => editTodo(index);
+        editIcon.onclick = () => editTodoText(index);
 
         const todoText = document.createElement('span');
         todoText.innerText = todo.text;
 
-        const label = document.createElement('span');
-        label.innerText = todo.status;
-        label.classList.add('label', todo.status.toLowerCase());
-        label.onclick = () => toggleStatus(index);
+        const statusButton = document.createElement('button');
+        statusButton.innerText = todo.status === 'New' ? 'Done' : 'New';
+        statusButton.style.backgroundColor = todo.status === 'New' ? 'red' : 'green';
+        statusButton.onclick = () => toggleStatus(index);
 
-        todoItem.appendChild(todoText);
-        todoItem.appendChild(editIcon);
-        todoItem.appendChild(label);
+        todoTextContainer.appendChild(todoText);
+        todoTextContainer.appendChild(editIcon);
+
+        todoItem.appendChild(todoTextContainer);
+        todoItem.appendChild(statusButton);
 
         todoList.appendChild(todoItem);
     });
+}  
+
+function editTodoText(index) {
+    const todoTextContainer = document.querySelector(`#todo-list li:nth-child(${index + 1}) .text-container`);
+    
+    const inputField = document.createElement('input');
+    inputField.type = 'text';
+    inputField.value = todos[index].text;
+    
+    inputField.addEventListener('blur', () => finishEditing(index));
+    inputField.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            finishEditing(index);
+        }
+    });
+
+    todoTextContainer.replaceChild(inputField, todoTextContainer.querySelector('span'));
+
+    inputField.focus();
 }
 
-function editTodo(index) {
-    const updatedText = prompt('Edit your todo:', todos[index].text);
-    if (updatedText !== null) {
+function finishEditing(index) {
+    const todoTextContainer = document.querySelector(`#todo-list li:nth-child(${index + 1}) .text-container`);
+    const updatedText = todoTextContainer.querySelector('input').value.trim();
+    
+    if (updatedText !== '') {
         todos[index].text = updatedText;
-        updateTodoList();
     }
 }
 
@@ -55,6 +88,5 @@ function toggleStatus(index) {
     todos[index].status = todos[index].status === 'New' ? 'Done' : 'New';
     updateTodoList();
 }
-
 
 updateTodoList();
